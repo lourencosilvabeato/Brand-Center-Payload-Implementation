ed20966fdebd1582857eff235b394ba43198bcc8
lourencosilvabeato-blip <lourenco.silva.beato@gmail.com>
2026-05-14 10:50:11 +0100

    fix: use getSessionUser() instead of fake Request in change-password page

--- src/app/(platform)/change-password/page.tsx
+++ src/app/(platform)/change-password/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
 import { getPayload } from '@/lib/payload'
-import { getSessionUserFromRequest } from '@/lib/auth'
+import { getSessionUser } from '@/lib/auth'
 import type { LoginSetting } from '@/payload-types'
 import { ChangePasswordForm } from '@/components/auth/ChangePasswordForm'
 import styles from './change-password.module.css'
@@ -9,16 +8,7 @@ import styles from './change-password.module.css'
 export const dynamic = 'force-dynamic'
 
 export default async function ChangePasswordPage() {
-  const cookieStore = await cookies()
-  const cookieHeader = cookieStore
-    .getAll()
-    .map((c) => `${c.name}=${c.value}`)
-    .join('; ')
-
-  const fakeRequest = new Request('http://localhost', {
-    headers: { cookie: cookieHeader },
-  })
-  const user = getSessionUserFromRequest(fakeRequest)
+  const user = await getSessionUser()
 
   if (!user || user.collection !== 'externalUsers') {
     redirect('/')
