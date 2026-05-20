1cc5df3539a3d89d51445e85e3df577e5dc002fc
Contributor <contributor@example.com>
2026-05-20 19:17:47 +0100

    Fixed migration index.ts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20260513_101302 from './20260513_101302';
-import * as migration_20260520_145510 from './20260520_145510';
+import * as migration_20260512_180323 from './20260512_180323';
 
 export const migrations = [
   {
-    up: migration_20260513_101302.up,
-    down: migration_20260513_101302.down,
-    name: '20260513_101302',
-  },
-  {
-    up: migration_20260520_145510.up,
-    down: migration_20260520_145510.down,
-    name: '20260520_145510'
+    up: migration_20260512_180323.up,
+    down: migration_20260512_180323.down,
+    name: '20260512_180323',
   },
 ];
